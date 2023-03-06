07aac37a3e8764a4c5e774ee6c71ba1edf6862bc
Joshua Rodriguez <rodriguez.taco.joshua@gmail.com>
2023-03-06 17:54:43 +0400

    HW01 finished Footer, Header and MainContent added

---
+++ src/components/MainContent/index.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+  height: 100vh;
+  background-color: #ccc;
+`;
+
+const MainContent = () => {
+  return (
+    <Container>
+      <p>sample</p>
+    </Container>
+  );
+};
+
+export default MainContent;
