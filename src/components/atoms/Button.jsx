ea608683cb09e501e712517445d24e88d71716c9
Joshua Rodriguez <joshua.rodriguez@flatstack.com>
2023-05-15 15:26:40 +0400

    added scroll to top button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components";
 
-const Button = ({ children, name, handleOnClick, isDisabled, $color }) => {
+const Button = ({ children, name, handleOnClick, isDisabled, $color, isHidden }) => {
   const ButtonContainer = styled.button(
     ({ color }) => css`
       padding: 10px 20px;
@@ -9,6 +9,7 @@ const Button = ({ children, name, handleOnClick, isDisabled, $color }) => {
       border: none;
       background: ${color};
       cursor: pointer;
+      ${isHidden ? "display: none" : ""}
     `,
   );
 
--- src/components/templates/index.jsx
+++ src/components/templates/index.jsx
@@ -3,7 +3,6 @@ import TableRow from "../molecules/TableRow";
 import Table from "../organisms/Table";
 
 const Index = () => {
-  // emulate a response from API of a json of projects
   const [projects] = useState([
     {
       id: 1,
@@ -25,6 +24,90 @@ const Index = () => {
     },
     {
       id: 4,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 5,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 6,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 7,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 8,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 9,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 10,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 11,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 12,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 14,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 15,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 16,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 17,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 18,
+      title: "My fourt project",
+      description: "testing propuse",
+      createdAt: Date.now(),
+    },
+    {
+      id: 19,
       title: "My fourt project",
       description: "testing propuse",
       createdAt: Date.now(),
